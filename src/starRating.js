a342a849594db2f2ff2990aa51716f65937f6c1a
Aman <aman13xrestha@gmail.com>
2021-05-27 11:54:29 +0545

    first commit

---
+++ src/starRating.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaStar } from "react-icons/fa";
+
+// individual star component
+const Star = ({selected = false}) => (
+	<FaStar color={selected ? "red" : "grey"} />
+);
+
+/*
+export default function StarRating() {
+	return [
+		<FaStar color="red" />,
+		<FaStar color="red" />,
+		<FaStar color="red" />,
+		<FaStar color="grey" />,
+		<FaStar color="grey" />
+			
+	];
+}
+*/
+
+const createArray = length => [...Array(length)];
+
+export default function StarRating({ totalStars = 5 }) {
+	return createArray(totalStars).map(
+		(n,i) => <Star key={i} />
+	);
+}

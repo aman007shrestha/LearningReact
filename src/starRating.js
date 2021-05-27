f3dde1c1b700d62acec9cae3e3ac37d24242194a
Aman <aman13xrestha@gmail.com>
2021-05-27 13:42:42 +0545

    set state using hook

--- src/starRating.js
+++ src/starRating.js
@@ -22,21 +22,29 @@ export default function StarRating() {
 const createArray = length => [...Array(length)];
 
 export default function StarRating({ totalStars = 5 }) {
-	const [selectedStars] = useState(3);
+	const [selectedStars, setSelectedStars] = useState(0);
 	console.log(selectedStars)
+	console.log(typeof selectedStars)
+	console.log(typeof setSelectedStars)
 
 	return (
 		<>
 			{
 				createArray(totalStars).map(
 					(n, i) => (
-						<Star key={i} selected={selectedStars > i} />
+						<Star 
+							key={i} 
+							selected={selectedStars > i}
+							onSelect={()=> setSelectedStars(i+1)}
+
+						/>
 					)
 				)
 			}
 			<p>
 				{selectedStars} of {totalStars} Stars
 			</p>
+			<button onClick={()=> setSelectedStars(0)}>Restart</button>
 		</>
 
 	);
